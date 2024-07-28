697161f6efeeed00866dd15b79d69ec5f0e9837e
Divyansh Dixir <divyanshdixit96@gmail.com>
2024-07-29 01:14:19 +0530

    response changes final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ import data from "../mocks/data.json";
 import Image from "../containers/Image";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Drawer from '@mui/material/Drawer';
+import Drawer from "@mui/material/Drawer";
 
 import { Box, List, ListItem } from "@mui/material";
+import { useMemo } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,6 +18,13 @@ const Navbar = () => {
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
+
+  const menu = useMemo(() => {
+    return {
+      keys: Object.keys(data.menuItems),
+      values: Object.values(data.menuItems),
+    };
+  }, []);
 
   return (
     <nav className="navbar">
@@ -27,33 +35,20 @@ const Navbar = () => {
       <div className="navdiv">
         <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
           <Box sx={{ width: 250 }} onClick={toggleDrawer(false)}>
-          <List className="drawer-list">
-          <ListItem>
-            <NavLink to="/"> Home </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/services"> Services </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/contact"> Contact </NavLink>
-          </ListItem>
-          {/* <li>
-            <NavLink to="/services">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact us</NavLink>
-          </li> */}
-          </List>
-          {/* <List>
-              <ListItem  disablePadding>
-                <NavLink to="/"> Home </NavLink>
-                <NavLink to="/services">Services</NavLink>
-                <NavLink to="/contact">Contact us</NavLink>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
+            <List className="drawer-list">
+              {menu.values.map((item, index) => (
+                <ListItem>
+                  <NavLink to={`/${menu.keys[index]}`}> {item} </NavLink>
+                </ListItem>
+              ))}
+              <ListItem>
+                <Button
+                  className="demo_btn"
+                  text={data.button.request}
+                  onclick={() => navigate("/contact")}
+                />
               </ListItem>
-          </List> */}
+            </List>
           </Box>
         </Drawer>
         <IconButton
@@ -67,18 +62,18 @@ const Navbar = () => {
           <MenuIcon />
         </IconButton>
         <ul>
-          <li>
-            <NavLink to="/"> Home </NavLink>
-          </li>
-          <li>
-            <NavLink to="/services">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact us</NavLink>
-          </li>
+          {menu.values.map((item, index) => (
+            <li>
+              <NavLink to={`/${menu.keys[index]}`}> {item} </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
-      <Button className="demo_btn" text={data.button.request} onclick={() => navigate("/contact")} />
+      <Button
+        className="demo_btn"
+        text={data.button.request}
+        onclick={() => navigate("/contact")}
+      />
     </nav>
   );
 };
